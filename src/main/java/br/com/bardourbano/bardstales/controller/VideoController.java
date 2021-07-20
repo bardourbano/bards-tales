6202e7d37c46c991afb7213c20f0e6abbb75730f
João Gabriel Cunha Melo <joao.gcmelo@outlook.com>
2021-07-20 19:46:18 -0300

    Added Patch endpoint and some small tweeks

--- src/main/java/br/com/bardourbano/bardstales/controller/VideoController.java
+++ src/main/java/br/com/bardourbano/bardstales/controller/VideoController.java
@@ -2,9 +2,11 @@ package br.com.bardourbano.bardstales.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +40,7 @@ public class VideoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Video store(@RequestBody Video video) {
         return repository.save(video);
     }
@@ -49,8 +51,8 @@ public class VideoController {
      * @param video
      * @return
      */
-    @PutMapping(path = {"/{id}"})
-    public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody Video video) {
+    @PutMapping(path = {"/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> putUpdate(@PathVariable long id, @RequestBody Video video) {
         return repository.findById(id)
                 .map(record -> {
                     record.setTitulo(video.getTitulo());
@@ -64,8 +66,31 @@ public class VideoController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @PatchMapping(path = {"/{id}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> patchUpdate(@PathVariable long id, @RequestBody Video video) {
+        return repository.findById(id)
+                .map(record -> {
+                    if (!(video.getTitulo().isEmpty() || video.getTitulo() == record.getTitulo())) {
+                        record.setTitulo(video.getTitulo());
+                    }
+
+                    if (!(video.getDescricao().isEmpty() || video.getDescricao() == record.getDescricao())) {
+                        record.setDescricao(video.getDescricao());
+                    }
+
+                    if (!(video.getUrl().isEmpty() || video.getUrl() == record.getUrl())) {
+                        record.setUrl(video.getUrl());
+                    }
+
+                    Video updatedVideo = repository.save(record);
+
+                    return ResponseEntity.ok().body(updatedVideo);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @DeleteMapping(path = {"/{id}"})
-    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+    public ResponseEntity<?> delete(@PathVariable long id) {
         return repository.findById(id)
                 .map(record -> {
                     repository.deleteById(id);
